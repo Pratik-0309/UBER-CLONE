12df323c7ec2508db543498eeee3396a9c64a53b
PratikParate <paratepratik10@gmail.com>
2026-04-20 17:11:01 +0530

    feat: add user and captain routes with registration, login, profile, and logout functionalities

---
+++ Backend/routes/captain.route.js
@@ -0,0 +1,31 @@
+import express from "express"
+import { body } from "express-validator";
+import {captainRegister, captainLogin, captainProfile, captainLogout} from "../controllers/captain.controller.js"
+import { refreshAccessToken } from "../controllers/user.controller.js";
+import {authCaptain} from "../middleware/auth.middleware.js"
+
+const captainRouter = express.Router();
+
+captainRouter.post("/register",[
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('fullName.firstName').isLength({min: 3}).withMessage('first name must be at least 3 character long'),
+    body('fullName.lastName').isLength({ min:3 }).withMessage('Last name must be at least 3 character long'),
+    body('password').isLength({min: 6}).withMessage('Password must be atleast 6 character long'),
+    body('vehicle.color').isLength({min: 3}).withMessage('vehicle color must be atleast 3 character long'),
+    body('vehicle.plate').isLength({min: 3}).withMessage('vehicle Number must be atleast 3 character long'),
+    body('vehicle.capacity').isInt({min: 1}).withMessage('vehicle capacity must be atleast 1'),
+    body('vehicle.vehicleType').isIn(['car', 'motorcycle', 'auto']).withMessage('Invalid Vehicle type')
+],captainRegister)
+
+captainRouter.post("/login",[
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({min: 6}).withMessage('Password must be atleast 6 character long'),
+],captainLogin)
+
+captainRouter.post("/refresh-token", refreshAccessToken);
+
+captainRouter.get("/profile", authCaptain, captainProfile);
+
+captainRouter.post("/logout", authCaptain, captainLogout);
+
+export default captainRouter;
---
+++ Backend/routes/user.route.js
@@ -0,0 +1,32 @@
+import express from "express";
+import { registerUser,loginUser, refreshAccessToken, userProfile, logoutUser  } from "../controllers/user.controller.js";
+import { body } from "express-validator";
+import authUser from "../middleware/auth.middleware.js";
+
+const userRouter = express.Router()
+
+userRouter.post("/register", [
+    body('email').isEmail().withMessage("Invalid Email"),
+    body('fullName.firstName').isLength({ min:3 }).withMessage(
+        'First name must be at least 3 character long'
+    ),
+    body('fullName.lastName').isLength({ min:3 }).withMessage(
+        'Last name must be at least 3 character long'
+    ),
+    body('password').isLength({min: 6}).withMessage('Password must be atleast 6 character long'),
+], registerUser);
+
+
+userRouter.post("/login",[
+    body('email').isEmail().withMessage("Invalid Email"),
+    body('password').isLength({min: 6}).withMessage('Password must be atleast 6 character long'),
+],loginUser)
+
+userRouter.post("/refresh-token", refreshAccessToken);
+
+userRouter.get("/profile", authUser, userProfile);
+
+userRouter.post("/logout", authUser, logoutUser);
+
+
+export default userRouter;
